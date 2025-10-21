461ceeaf0036323fe4b747ff0bc663b08fe94396
IsaacAyaC1FC0 <isaac.ayala@cifco.gob.sv>
2025-10-21 15:21:49 -0600

    Adición de vista y cambio en la tabla de tickets

---
+++ database/migrations/2025_10_25_104728_create_views_v_station_tickets_and_v_tickets.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // Primero eliminamos las vistas si ya existen
+        DB::statement('DROP VIEW IF EXISTS v_station_tickets;');
+        DB::statement('DROP VIEW IF EXISTS v_tickets;');
+
+        //  Crear vista v_station_tickets
+        DB::statement("
+                CREATE VIEW v_station_tickets AS
+                SELECT
+                    pr.id AS product_id,
+                    st.id AS station_id,
+                    u.id AS user_id,
+                    st.station_name,
+                    pr.product_name,
+                    pr.unit_price,
+                    pr.cost,
+                    tc.uuid,
+                    tc.created_at
+                FROM
+                    transactions tr
+                JOIN stations st ON
+                    (st.id = tr.station_id)
+                JOIN station_users su ON
+                    (st.id = su.station_id)
+                JOIN users u ON
+                    (su.user_id = u.id)
+                JOIN tickets tc ON
+                    (tc.station_id = st.id)
+                JOIN products pr ON
+                    (pr.id = tc.product_id) ");
+
+        // Crear vista v_tickets
+        DB::statement("
+            CREATE VIEW v_tickets AS
+            SELECT
+                tc.id AS ticket_id,
+                tc.product_id AS product_id,
+                pr.product_name AS product_name,
+                tc.uuid AS uuid,
+                pr.unit_price AS unit_price,
+                tc.status AS status,
+                tc.created_at AS created_at
+            FROM tickets tc
+            JOIN products pr ON tc.product_id = pr.id
+        ");
+    }
+
+    public function down(): void
+    {
+        // Si hacemos rollback, eliminamos las vistas
+        DB::statement('DROP VIEW IF EXISTS v_station_tickets;');
+        DB::statement('DROP VIEW IF EXISTS v_tickets;');
+    }
+};
