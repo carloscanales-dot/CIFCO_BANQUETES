34554e3c768fa883b035b9b8edeee7f2d3746489
IsaacAyaC1FC0 <isaac.ayala@cifco.gob.sv>
2025-10-22 13:15:45 -0600

    eliminacion de relacion entre localización y feria

--- Modules/Ticket/database/migrations/2025_07_21_033952_create_location_table.php
+++ Modules/Ticket/database/migrations/2025_07_21_033952_create_location_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('location', function (Blueprint $table) {
             $table->id();
             $table->string('location_name', 65);
-            $table->foreignId('fair_id')->constrained('fairs')->onUpdate('cascade');
             $table->timestamps();
         });
     }
